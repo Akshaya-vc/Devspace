4ccbab5a41fbf54f8c7eb3d3ac6d3810d8f71e28
Akshaya Chelliah <72297207+Akshaya-vc@users.noreply.github.com>
2021-03-21 02:51:17 +0530

    Delete src directory

--- src/components/counter.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-class Counter extends Component {
-    render(){
-        return <div><h1>Hello here!! Im great -- inside component file</h1><button>hello</button></div>;
-    }
-}
- 
-export default Counter;
